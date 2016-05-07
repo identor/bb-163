fbc7334efd3d7753ee40087d50a08e8db6327386
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2016-05-07 19:10:17 +0800

    Login redirection.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,20 +1,31 @@
 export class MainController {
-  constructor ($ngmDashboard, Firebase, $firebaseObject, firebaseUrl) {
+  constructor ($ngmDashboard, $state, $appUserService, Firebase, $firebaseObject, firebaseUrl, $rootScope, $sessionStorage) {
     'ngInject';
 
+    this.user = $rootScope.user = $sessionStorage.user;
+
+    if (!this.user) {
+      $state.go('login');
+    }
+
     this.$ngmDashboard = $ngmDashboard;
+    this.$appUserService = $appUserService;
     this.$ngmDashboard.fabOnClick = () => { $ngmDashboard.toggleNav() };
 
     this.ref = new Firebase(firebaseUrl);
     this.mapCoords = $firebaseObject(this.ref.child('map'));
 
     this.mapCoords.$loaded().then((coords) => {
-      this.map = { center: { latitude: this.mapCoords.latitude, longitude: this.mapCoords.longitude }, zoom: 20 };
+      this.map = { center: { latitude: this.mapCoords.latitude, longitude: this.mapCoords.longitude }, zoom: 16 };
       this.marker = { id: Date.now(), coords: coords };
 
       this.mapCoords.$watch(() => {
         this.map.center = { latitude: this.mapCoords.latitude, longitude: this.mapCoords.longitude };
       });
     });
+  }
+
+  logout() {
+    this.$appUserService.logout();
   }
 }
--- src/app/utils/dashboard/nav.directive.js
+++ src/app/utils/dashboard/nav.directive.js
@@ -4,27 +4,28 @@ export function NavDirective() {
   let directive = {
     restrict: 'E',
     template: `
-      <md-sidenav ng-hide="vm.$ngmDashboard.isHideNav" class="md-sidenav-left md-whiteframe-z2" md-component-id="left" md-is-locked-open="$mdMedia('gt-sm')" layout="column">
+      <md-sidenav ng-hide="nav.$ngmDashboard.isHideNav" class="md-sidenav-left md-whiteframe-z2" md-component-id="left" md-is-locked-open="$mdMedia('gt-sm')" layout="column">
         <md-toolbar layout="row" layout-align="center center">
           <md-button>
             <span>BB 163</span>
           </md-button>
           <span flex></span>
-          <md-button class="md-icon-button" ui-sref="login">
+          <md-button class="md-icon-button" ng-click="console.log('hello')">
             <md-icon>exit_to_app</md-icon>
           </md-button>
         </md-toolbar>
         <md-list>
-          <md-list-item ng-click="true">
+          <md-list-item ng-click="console.log('Heasdfasdf')">
             <md-icon>location_on</md-icon>
-            <p>Locate</p>
+            <p>Locate device</p>
           </md-list-item>
         </md-list>
+        {{ vm }}
         <span flex></span>
       </md-sidenav>
     `,
     controller: NavController,
-    controllerAs: 'vm',
+    controllerAs: 'nav',
     bindToController: true,
     replace: true
   };
@@ -33,9 +34,14 @@ export function NavDirective() {
 }
 
 class NavController {
-  constructor ($ngmDashboard) {
+  constructor ($ngmDashboard, $appUserService) {
     'ngInject';
 
     this.$ngmDashboard = $ngmDashboard;
+    this.$appUserService = $appUserService;
+  }
+
+  logout() {
+    this.$appUserService.logout();
   }
 }
